47b2022a103ce59552340d38b4bd1e631b5025b3
protowerx-ai <protowerx.ai@gmail.com>
2024-05-14 12:05:41 -0500

    team page

--- pages/0 The Protowerx Team.py
+++ pages/0 The Protowerx Team.py
@@ -19,13 +19,13 @@ col1, col2, col3=  st.columns([1,1,1])
 with col1:
     st.image("img/lane_headshot.jpg")
     st.subheader("Lane Boyer")
-    st.write("Founder")
+    #st.write("Founder")
     st.write("As an AI/ML solutions data scientist/engineering manager, I leverage my experience and passion for field-testing new technology to design, architect, and build operational AI solutions / systems for real-world customer requirements. I have executed AI/ML development and engineering efforts across the energy and defense industries both domestically and abroad. I have led teams, projects, and people from AI concept to experimentation to deployment. I hold an M.S. in Artificial Intelligence from Northwester University and M.S and B.S. degrees in Geology from the University of Arkansas.")
 
     st.write("I enjoy competing in endurance running events and have run 14 marathons in the last 8 years with a PR of 2:27. I excel at working with a team to solve difficult problems and especially enjoy employing new technologies to do so.")
 
-with col2:
-    st.header("...")
+# with col2:
+#     st.header("...")
 
-with col3:
-    st.header("...")
+# with col3:
+#     st.header("...")
