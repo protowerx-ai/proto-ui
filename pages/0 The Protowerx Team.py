3731f6ed190cc58e045b24eb7ce7b29f1880f11c
laneboyer <35941948+laneboyer@users.noreply.github.com>
2024-05-24 16:32:06 -0500

    added derek to team page

--- pages/0 The Protowerx Team.py
+++ pages/0 The Protowerx Team.py
@@ -24,8 +24,11 @@ with col1:
 
     st.write("I enjoy competing in endurance running events and have run 14 marathons in the last 8 years with a PR of 2:27. I excel at working with a team to solve difficult problems and especially enjoy employing new technologies to do so.")
 
-# with col2:
-#     st.header("...")
+with col2:
+    st.image("img/derek.jpeg")
+    st.subheader("Derek Hanson PhD")
+    st.write("As a Senior Data Scientist at SparkCognition Government Systems (SGS), I develop and deploy cutting-edge AI and ML solutions for various government and defense clients. I have over 8 years of experience in using data science, analytics, and experimentation to improve technology products and services.")
 
+    st.write("I am passionate about leveraging AI and ML to solve complex and impactful problems, and to deliver value and innovation to the government and defense sector. I am always eager to learn new tools and techniques, and to collaborate with diverse and talented teams. I strive to create ethical, scalable, and robust AI and ML solutions that can enhance the security, efficiency, and performance of our clients.")
 # with col3:
 #     st.header("...")
